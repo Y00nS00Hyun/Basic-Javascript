67aad369037a2bb1dc54dc96b1e40b9da2163e68
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-08 03:57:41 +0900

    해설

--- 1/이벤트핸들러퀴즈.js
+++ 1/이벤트핸들러퀴즈.js
@@ -1,15 +1,14 @@
-//버튼을 클릭했을 떄 콘솔에 출력되는 결과는?
+//버튼을 클릭했을 때 콘솔에 출력되는 결과는?
 
 const btn = document.querySelector('#btn');
-
 function printBtn() {
     console.log(btn.outerHTML);
 }
 
-btn.onclick = function () {
+btn.onclick = function () {     //onclick1 - onclick2에 의해 덮어씌워짐
     console.log('click!');
 };
-btn.onclick = printBtn;
-btn.removeEventListener('click', printBtn);
+btn.onclick = printBtn;         //onclick2 - onclick1을 덮어씀
+btn.removeEventListener('click', printBtn);        //addEventListener가 없으므로 아무 의미 없는 코드
 
-//버튼을 클릭했을 떄 콘솔에 출력되는 결과는?
+//정답: <button id="btn">Click!</button>
