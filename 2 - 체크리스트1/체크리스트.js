6f17cf6e59b2c8933b4846386d3539a0115a17ec
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-08 05:48:06 +0900

    헷갈려

--- 2 - 체크리스트1/체크리스트.js
+++ 2 - 체크리스트1/체크리스트.js
@@ -1,9 +1,9 @@
 const toDoList = document.querySelector('#to-do-list');
 
 function addNewTodo(text) {
-    const add = document.createElement("li");
+    const add = document.createElement('li');
     add.textContent = text;
-    toDoList.append(add);
+    toDoList.prepend(add);
 }
 
 // 테스트 코드
