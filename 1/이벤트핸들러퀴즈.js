3c12402092e640e019f6f17a14005c438af562ad
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-08 04:13:25 +0900

    onclick 사용하기

--- 1/이벤트핸들러퀴즈.js
+++ 1/이벤트핸들러퀴즈.js
@@ -11,4 +11,4 @@ btn.onclick = function () {     //onclick1 - onclick2에 의해 덮어씌워짐
 btn.onclick = printBtn;         //onclick2 - onclick1을 덮어씀
 btn.removeEventListener('click', printBtn);        //addEventListener가 없으므로 아무 의미 없는 코드
 
-//정답: <button id="btn">Click!</button>
+//정답: <button id="btn"> Click! </button>
