39dec7d76558b4ae33a9cc699fea9d0eeb280066
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-08 18:00:07 +0900

    쥐돌이의 육아

---
+++ 8 - 쥐돌이/쥐돌이네_가족_정리.js
@@ -0,0 +1,6 @@
+// mouseenter는 mouseover처럼 마우스 포인터가 요소 바깥에서 안쪽으로 들어갈 때,
+// mouseleave는 mouseout처럼 마우스 포인터가 요소 안쪽에서 바깥으로 나갈 때
+
+
+// 이벤트 핸들러가 자식 요소에까지 영향을 끼치게 하고싶은 경우에는 mouseover / mouseout을,
+// 자식 요소에 영향을 끼치지 않고 해당 요소에만 이벤트 핸들러를 다루고자 한다면 mouseenter / mouseleave를 활용 
