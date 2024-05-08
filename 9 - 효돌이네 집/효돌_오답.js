2f3d895fd6ea72fccd6714cd4917a7fdbeedd69f
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-09 03:58:20 +0900

    나는야 천재 히히

--- 9 - 효돌이네 집/효돌_오답.js
+++ 9 - 효돌이네 집/효돌_오답.js
@@ -12,9 +12,12 @@ function showTitle(e) {
 
 // removeTitle 함수를 완성해 주세요
 function removeTitle(e) {
-    // 여기에 코드를 작성하세요
-
+    if (e.target.dataset.title) {
+        e.target.lastElementChild.remove();
+    }
 }
+
+//으하하하! 나 혼자 풀었지롱
 
 // '대상'과 '타입'을 수정해 주세요
 대상.addEventListener('타입', showTitle);
