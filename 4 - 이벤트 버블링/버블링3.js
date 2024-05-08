93c509c9eea5049f6e3b136c975006dd84b4c130
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-08 16:10:25 +0900

    부모태그로 핸들러 등록 후 이벤트 버블링 제한하기

--- 4 - 이벤트 버블링/버블링3.js
+++ 4 - 이벤트 버블링/버블링3.js
@@ -11,12 +11,20 @@ list.addEventListener('click', function (e) {
 //근데 그 부모태그를 눌러도 작동할 수 있기 때문에 contains 나 tagName 을 사용하여 적용에 제한을 둠
 
 
-const li = document.createElement('li');
-li.classList.add('item');
-li.textContent = '일기 쓰기';
-list.append(li);
-li.addEventListener('click', function (e) {
+const addlist = document.createElement('li');
+addlist.classList.add('item');
+addlist.textContent = '일기 쓰기';
+list.append(addlist);
+list.addEventListener('click', function (e) {
     e.stopPropagation();
 });
 
 
+/*
+for (let item of list.children){
+    item.addEventListener('click', function(e)){
+        e.target.classList.toggle('done');
+    }};
+    이렇게 이벤트 각각에 대해 핸들러 설정 X
+    추가했을 때 추가된 요소에 작동 안함
+*/
