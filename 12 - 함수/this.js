00c068ef092c40b8530f065d26b7a7b5e998cc92
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-16 02:07:26 +0900

    this 를 사용해서 유연성있게 선언하기

---
+++ 12 - 함수/this.js
@@ -0,0 +1,62 @@
+//arrow function보다는 일반 함수에 권장됨
+
+//기본
+
+const username = {
+    firstname: 'Yoon',
+    lastname: 'soohyun',
+    getFullName: function () {
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
+
+console.log(username.getFullName());
+
+
+
+
+//응용
+
+function animal() {
+    return `${this.head} ${this.tail}`;
+}
+
+const rabit = {
+    head: 'Yoon',
+    tail: 'soorim',
+    animal: animal,
+};
+
+const pig = {
+    head: 'Kim',
+    tail: 'midol',
+    animal: animal,
+    //animal()이 아닌 이유:
+    //함수를 객체의 메소드로서 바로 호출하려면 해당 객체가 완전히 정의된 후에 호출해야함
+    //function이 밖에 있으면 바로 호출은 못하는듯??? ...-_-
+};
+
+console.log(rabit.animal());
+console.log(pig.animal());
+
+
+
+//객체 출력하기
+
+function printAlphabet() {
+    console.log(this);  //this 를 출력하면 객체가 출력됨
+    console.log(`${this.content}`);
+}
+
+const A = {
+    content: 'a',
+    printA: printAlphabet,
+}
+
+const B = {
+    content: 'b',
+    printB: printAlphabet,
+}
+
+A.printA();
+B.printB();
