35de2a05beaca8c979a2a50c26515f12b6fd916b
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-16 08:09:56 +0900

    spread 를 사용해서 배열에 요소 추가하기

--- 13 - 문법/02_spread.js
+++ 13 - 문법/02_spread.js
@@ -5,7 +5,7 @@ console.log(...numbers);
 
 const fish = ['고등어', '연어'];
 const animal = fish.slice();    //배열 복사
-const animal2 = [...fish, '나비', '뱀'];
+const animal2 = [...fish, '나비', '전갈'];
 
 animal.push('치타');
 
