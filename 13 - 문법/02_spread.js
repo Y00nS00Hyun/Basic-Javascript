6b342f8c2d0adb6c91266a5d60e6e067f9b5855d
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-16 08:09:12 +0900

    함수 이름 바꾸기

---
+++ 13 - 문법/02_spread.js
@@ -0,0 +1,14 @@
+const numbers = [100, 101, 102];
+console.log(...numbers);
+
+//
+
+const fish = ['고등어', '연어'];
+const animal = fish.slice();    //배열 복사
+const animal2 = [...fish, '나비', '뱀'];
+
+animal.push('치타');
+
+console.log(fish);
+console.log(animal);
+console.log(animal2);
