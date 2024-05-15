21f37df14396e63e7f78a6c622fb0abc7740b9f7
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-16 08:58:55 +0900

    생략 표기법

---
+++ 13 - 문법/05_생략표기법.js
@@ -0,0 +1,21 @@
+const title = '어린왕자';
+const hero = '왕자';
+const friend = '여우';
+
+const book = {
+    title,
+    hero,
+    friend,
+    page: '502p'
+};
+
+const book2 = {
+    title: title,
+    hero: hero,
+    friendf: friend,
+    page: '502p'
+};
+
+console.log(book, book2);
+
+//2처럼 안써도 된다
