df0a428033eecf8e702e6bf777f028eaed5e1e15
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-19 21:42:56 +0900

    왜 안되지?

---
+++ 13 - 문법/18_try_catch.js
@@ -0,0 +1,14 @@
+try {
+  console.log("에러 전");
+
+  const soorim = "바보";
+  console.log(soorim);
+
+  const soorim = "천사"; //에러-_-
+
+  const soohyun = "천재";
+  console.log(soohyun);
+} catch (error) {
+  console.log("에러 발생");
+  console.log(error);
+}
