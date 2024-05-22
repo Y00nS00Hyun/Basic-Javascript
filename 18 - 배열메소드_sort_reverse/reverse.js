c9c55c1e7a455e7c9a6ed1a6e8131a2693d921a8
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-22 11:20:39 +0900

    aaa.reverse()

--- 18 - 배열메소드_sort_reverse/reverse.js
+++ 18 - 배열메소드_sort_reverse/reverse.js
@@ -2,11 +2,7 @@
 
 const numbers = [4, 2, 5, 1, 3];
 numbers.sort((a, b) => a - b);
-
 console.log(numbers); // [1, 2, 3, 4, 5]
 
 numbers.reverse();
-
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i] + numbers[i]);
-}
+console.log(numbers); // [5, 4, 3, 2, 1]
