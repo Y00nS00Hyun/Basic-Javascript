2109744d36771704561e7b9297d2461ca80f5ab7
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-24 13:43:01 +0900

    모듈

--- 21 - 모듈/index.js
+++ 21 - 모듈/index.js
@@ -1,8 +1,9 @@
 import { title as soohyuntitle, soohyunSay } from "./printer.js";
-import * as members from "./members.js";
+import babo, * as members from "./members.js";
 
 const title = "swim 참방참방";
 
 soohyunSay(title);
 soohyunSay(soohyuntitle);
 console.log(members.data);
+console.log(babo); //default
