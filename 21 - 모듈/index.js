fe3541f5dbf57958db727b16affa2f78f8eb72ba
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-05-24 13:00:05 +0900

    와일드카드 문자 *

--- 21 - 모듈/index.js
+++ 21 - 모듈/index.js
@@ -1,5 +1,8 @@
 import { title as soohyuntitle, soohyunSay } from "./printer.js";
+import * as members from "./members.js";
 
 const title = "swim 참방참방";
 
 soohyunSay(title);
+soohyunSay(soohyuntitle);
+console.log(members.data);
